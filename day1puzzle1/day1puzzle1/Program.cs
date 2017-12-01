914362169c961005b4b837a54d56c978770404f3
unknown <alra3356@im-ts05.user.uu.se>
2017-12-01 22:15:22 +0100

    Removed some spaghetti

---
+++ day1puzzle1/day1puzzle1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1puzzle1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string text = System.IO.File.ReadAllText(@"X:\Sysvet\adventOfCode\input.txt");
+
+            Console.WriteLine(p.Prg1(text));
+            Console.WriteLine(p.Prg2(text));
+            Console.ReadKey();
+        }
+        public int Prg1(string text)
+        {
+            string textOut = "";
+            int count = 0, current = 0, next = text.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                current = i;
+                if (i == text.Length - 1)
+                {
+                    next = 0;
+                }
+                else
+                {
+                    next = i + 1;
+                }
+
+                if (text.Substring(current, 1) == text.Substring(next, 1))
+                {
+                    count = count + Int32.Parse(text.Substring(i, 1).ToString());
+
+                }
+            }
+            return count;
+        }
+
+        public int Prg2(string text)
+        {
+            string textOut = "";
+            int count = 0, current = 0, next = text.Length;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                current = i;
+                if (current < text.Length/2)
+                {
+                    next = current + text.Length/2;
+                }
+                else
+                {
+                    next = current + text.Length/2 - text.Length;
+                }
+
+                if (text.Substring(current, 1) == text.Substring(next, 1))
+                {
+                    count = count + Int32.Parse(text.Substring(i, 1).ToString());
+                }
+            }
+            return count;
+        }
+    }
+}
