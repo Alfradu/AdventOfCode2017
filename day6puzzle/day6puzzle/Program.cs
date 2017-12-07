8f0bf1093b506b63f96360da5a6db8e8460c962e
unknown <alra3356@im-ts05.user.uu.se>
2017-12-07 04:09:16 +0100

    Solutions day 6

---
+++ day6puzzle/day6puzzle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day6puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"X:\Sysvet\adventOfCode\day6puzzle\input.txt");
+            int[] instructions = text.Split(new char[] { '\t' }).Select(x => int.Parse(x)).ToArray();
+
+            int[] answer = Pgm(instructions);
+            Console.WriteLine(answer[0]);
+            Console.WriteLine(answer[1]);
+
+            Console.ReadKey();
+        }
+
+        private static int[] Pgm(int[] instructions)
+        {
+            int pickedValue;
+            int index;
+            List<int[]> recordedStates = new List<int[]>();
+
+            while (!recordedStates.Any(x => x.SequenceEqual(instructions)))
+            {
+                recordedStates.Add(instructions.ToArray());
+                pickedValue = instructions.Max();
+                index = Array.IndexOf(instructions, pickedValue);
+                instructions[index] = 0;
+                for (int i = 1; i <= pickedValue; i++)
+                {
+                    instructions[(index+i) % instructions.Length]++;
+                }
+
+            }
+            return new int[] {recordedStates.Count(), recordedStates.Count() - recordedStates.FindIndex(instructions.SequenceEqual)};  
+        }
+    }
+}
